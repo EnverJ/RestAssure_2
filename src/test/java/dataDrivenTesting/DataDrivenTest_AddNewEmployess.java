eca3f4699f23a9892ea4ba6773340b1a0ed01079
EnverJ <Enlem1987@gmail.com>
2022-07-23 23:40:51 -0600

    data driven testing

--- src/test/java/dataDrivenTesting/DataDrivenTest_AddNewEmployess.java
+++ src/test/java/dataDrivenTesting/DataDrivenTest_AddNewEmployess.java
@@ -6,18 +6,21 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataDrivenTest_AddNewEmployess {
-   @Test
-    void addNewEmployees(){
+   @Test(dataProvider = "empDataProvide")
+    void addNewEmployees(String firstName,String lastName,String subjectId){
+       // hard coded data driven. single set of data
+
         RestAssured.baseURI="http://localhost:3000";
         RequestSpecification httpRequest=RestAssured.given();
         // here we created data which we can send along with post request
         JSONObject requestParams=new JSONObject();
-        requestParams.put("firstName","fslkg");
-        requestParams.put("lastName","Enver");
-        requestParams.put("subjectId",2);
+        requestParams.put("firstName",firstName);
+        requestParams.put("lastName",lastName);
+        requestParams.put("subjectId",subjectId);
 
         // Add a header stating the body Request body in a JSON
         httpRequest.header("Content-Type","application/json");
@@ -28,13 +31,20 @@ public class DataDrivenTest_AddNewEmployess {
         Response response=httpRequest.request(Method.POST,"/user");
         //capture the response and perform validation
         String responseBody=response.getBody().asString();
+       System.out.println("responseBody: "+responseBody);
         // verify
-        Assert.assertEquals(responseBody.contains("fslkg"),true);
+    /*    Assert.assertEquals(responseBody.contains("fslkg"),true);
         Assert.assertEquals(responseBody.contains("Enver"),true);
         Assert.assertEquals(responseBody.contains("2"),true);
         int statusCode=response.getStatusCode();
-        Assert.assertEquals(statusCode,201);
+        Assert.assertEquals(statusCode,201);  */
 
+    }
+    //2 postReqt---multiple set of data
+    @DataProvider(name="empDataProvide")
+    Object[][] getEmpData(){
+       String empData[][]={{"abs","Enver","3"},{"sadf","Enver","6"},{"Eddd","Enver","4"}};
+       return (empData);
 
     }
 }
